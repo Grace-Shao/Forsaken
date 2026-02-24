5856537a156e0e7571da82c9e4465d2409e42447
rvo7 <rvo7@gatech.edu>
2026-02-24 13:31:35 -0500

    del irrelevant files

--- Assets/Scripts/PlayerBarsSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlayerBars", menuName = "ScriptableObjects/PlayerBars", order = 1)]
-public class PlayerBarsSO : ScriptableObject
-{
-    public float health = 100f;
-    public float mana = 100f;
-    public float xp = 0f;
-}
