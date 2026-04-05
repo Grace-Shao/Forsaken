8873bf4003a939cf8380a84f40dad62d206288e6
Arshiya Rahman <127697399+arahman302@users.noreply.github.com>
2026-04-05 16:56:17 -0400

    parry timing

--- Assets/Scripts/Boss/BossStateMachine.cs
+++ Assets/Scripts/Boss/BossStateMachine.cs
@@ -55,6 +55,7 @@ public class BossStateMachine : StateMachine, IDamageable
 
     #region VFX
     private ParticleSystem damageTakenParticles;
+    private ParticleSystem attackIndicator;
     #endregion
     
     #region Getters and Setters
@@ -127,6 +128,7 @@ public class BossStateMachine : StateMachine, IDamageable
         sprite = transform.Find("Sprite");
         Health = 100;
         damageTakenParticles = sprite.Find("hit received particles").GetComponent<ParticleSystem>();
+        attackIndicator = sprite.Find("Broadsword").Find("ShootPoint").Find("Attack Indicator").GetComponent<ParticleSystem>();
     }
 
     protected override void EnterBeginningState()
@@ -223,9 +225,14 @@ public class BossStateMachine : StateMachine, IDamageable
 
     public void OnLaserAttackEnd()
     {
-        Debug.Log("lasers finished");
         lasersFinished = 1;
 
+    }
+
+    public void AttackIndicator()
+    {
+        Debug.Log("attack");
+        attackIndicator.Play();
     }
     #endregion
  
